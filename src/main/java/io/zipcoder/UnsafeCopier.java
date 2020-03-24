af613bbb0606107df60dec74379308c1d4d808db
aartikansal <aartilumb@gmail.com>
2020-03-24 18:11:36 -0400

    changes to unsafe copier

--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.Random;
+
 /**
  * Modify the run function so that the monkeys each grab the next word and write it to the copy.
  */
@@ -10,5 +12,14 @@ public class UnsafeCopier extends Copier {
     }
 
     public void run() {
+        StringBuilder builder = new StringBuilder();
+        while(stringIterator.hasNext()) {
+
+            String word = stringIterator.next();
+
+            builder.append(word + " ");
+        }
+
+        copied += builder.toString();
     }
 }
